d9b569fd4e5454c9555072b157e8866c94c41bc3
Vital Zakharau <v.zaharov@21vek.by>
2022-07-13 01:02:13 +0300

    Redis

--- Redis/g_hsets.php
+++ Redis/g_hsets.php
@@ -2,5 +2,5 @@
 
 for ($i = 1; $i <= 200000; $i++) {
     $data = 'HSET hset:'. $i . ' "colour" "blue" "make" "saab" "model" 93 "features" "powerlocks,moonroof" ' . PHP_EOL;
-    file_put_contents('./datasets/hset.txt', $data, FILE_APPEND);
+    file_put_contents('./datasets/hsets.txt', $data, FILE_APPEND);
 }
--- Redis/g_zsets.php
+++ Redis/g_zsets.php
@@ -5,6 +5,6 @@ for ($i = 1; $i <= 200000; $i++) {
 {"colour":"blue","make":"saab","model":$i,"features":["powerlocks","moonroof"]}
 JSON;
     $data = 'ZADD zset ' . $i . ' "' . addslashes($json) . '"' . PHP_EOL;
-    file_put_contents('./datasets/zset.txt', $data, FILE_APPEND);
+    file_put_contents('./datasets/zsets.txt', $data, FILE_APPEND);
 }
 
